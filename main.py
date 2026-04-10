c81da07821a1833b53a36af4a0e7ebf99990ac57
Handoyo-Gunawan-dev <handoyo.gunawan84@gmail.com>
2026-04-10 10:02:13 +0700

    Day 2: simpan transaksi ke JSON

--- main.py
+++ main.py
@@ -10,3 +10,48 @@ print("=== DATA TRANSAKSI ===")
 for t in transaksi:
     total=t["qty"]*t["harga"]
     print(f"{t['produk']} | qty: {t['qty']} | total: {total}")
+
+import json
+import os
+
+FILE_PATH = "database/transaksi.json"
+
+# fungsi baca data
+def load_data():
+    if not os.path.exists(FILE_PATH):
+        return []
+    with open(FILE_PATH, "r") as file:
+        return json.load(file)
+
+# fungsi simpan data
+def save_data(data):
+    with open(FILE_PATH, "w") as file:
+        json.dump(data, file, indent=4)
+
+# tambah transaksi
+def tambah_transaksi(produk, qty, harga):
+    data = load_data()
+    
+    transaksi_baru = {
+        "id": len(data) + 1,
+        "produk": produk,
+        "qty": qty,
+        "harga": harga
+    }
+    
+    data.append(transaksi_baru)
+    save_data(data)
+
+# tampilkan transaksi
+def tampilkan_transaksi():
+    data = load_data()
+    print("\n=== DATA TRANSAKSI ===")
+    for t in data:
+        total = t["qty"] * t["harga"]
+        print(f"{t['produk']} | Qty: {t['qty']} | Total: {total}")
+
+# TEST
+tambah_transaksi("Beras", 2, 10000)
+tambah_transaksi("Minyak", 1, 15000)
+
+tampilkan_transaksi()
